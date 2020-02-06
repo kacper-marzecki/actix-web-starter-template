c25d3689c244362e0fb96cf35ce5f6010dc5c7ec
kacper-marzecki <60017757+kacper-marzecki@users.noreply.github.com>
2020-02-06 22:05:26 +0100

    started adding validator

--- src/service/user/request.rs
+++ src/service/user/request.rs
@@ -1,6 +1,13 @@
-#[derive(Debug, Deserialize)]
+use validator::{Validate, ValidationError};
+
+
+#[derive(Debug, Validate, Deserialize)]
 pub struct RegisterUserRequest {
+    #[validate(email)]
     pub email: String,
+    #[validate(length(min = 1))]
     pub password: String,
+    #[validate(length(min = 1))]
+    pub password_repeat: String
 }
 
